c20a72941e3b9b1d10e81e136f47be7ccc15cf17
Nimalansiv <sivakkumarnimalan@gmail.com>
2025-08-19 09:25:25 +0530

    Delete Interfaces directory

--- Interfaces/IBusRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using FastxWebApi.Models;
-
-namespace FastxWebApi.Interfaces
-{
-    public interface IBusRepository:IRepository<int,Bus>
-    {
-        public Task<IEnumerable<Bus>> GetAvailableBuses();
-    }
-}
